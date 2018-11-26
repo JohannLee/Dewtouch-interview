fb39775e24f3be1f035f5de17214374a0a7d55b2
JohannLee <elwinjohann@gmail.com>
2018-11-26 12:17:18 +0800

    Completion of Qustion 6

--- app/Controller/MigrationController.php
+++ app/Controller/MigrationController.php
@@ -9,8 +9,6 @@ class MigrationController extends AppController{
 
         $this->setFlash('Question: Migration of data to multiple DB table');
 
-        echo "<pre>";
-
         $this->loadModel('Member');
         $this->loadModel('Transaction');
         $this->loadModel('TransactionItem');
@@ -20,8 +18,6 @@ class MigrationController extends AppController{
                     $file = $this->request->data['MigrationFile']['file'];
 
                     if (is_file($file['tmp_name'])){
-                        // check extension
-                        $extension = substr(strtolower(strrchr($file['name'], '.')), 1);
                         // get the last id of Member and Transaction
                         $lastMember = $this->Member->find('first', array('order' => array('Member.id' => 'DESC'), 'recursive' => -1))['Member']['id'];
                         $currMemberID = $lastMember;
@@ -33,6 +29,8 @@ class MigrationController extends AppController{
                         $transaction_to_save = array();
                         $transaction_item_to_save = array();
 
+                        // check extension
+                        $extension = substr(strtolower(strrchr($file['name'], '.')), 1);
                         if ($extension == 'xlsx') {
                             $reader = IOFactory::createReader("Xlsx");
                             $spreadsheet = $reader->load($file['tmp_name']);
@@ -55,19 +53,18 @@ class MigrationController extends AppController{
                                     'recursive' => -1,
                                 ));
                                 if (empty($curr_member)) {
-                                    $member_to_save[$i]['type'] = $membertype;
-                                    $member_to_save[$i]['no'] = $memberno;
+                                    $member_to_save[$i]['type'] = $member_type;
+                                    $member_to_save[$i]['no'] = $member_no;
                                     $member_to_save[$i]['name'] = $membername;
                                     $member_to_save[$i]['company'] = $sheetdata[$i]['F'];
-                                    
+
                                     $lastMember++;
                                     $currMemberID = $lastMember;
                                 }
                                 else {
-                                    $currMemberID = $curr_member['id'];
-                                    //print_r($curr_member);
+                                    $currMemberID = $curr_member['Member']['id'];
                                 }
-                                
+
                                 $receipt_no = $sheetdata[$i]['I'];
                                 //check if current transaction exists (assuming receipt number is unique)
                                 $curr_transaction = $this->Transaction->find('first', array(
@@ -77,7 +74,7 @@ class MigrationController extends AppController{
                                     'recursive' => -1,
                                 ));
                                 if (empty($curr_transaction)) {
-                                    $transaction_to_save[$i]['member_id'] = 0;
+                                    $transaction_to_save[$i]['member_id'] = $currMemberID;
                                     $transaction_to_save[$i]['member_name'] = $sheetdata[$i]['C'];
                                     $transaction_to_save[$i]['member_paytype'] = $sheetdata[$i]['E'];
                                     $transaction_to_save[$i]['member_company'] = $sheetdata[$i]['F'];
@@ -85,8 +82,8 @@ class MigrationController extends AppController{
                                     $date = date('Y-m-d' ,strtotime($sheetdata[$i]['A']));
                                     $transaction_to_save[$i]['date'] = $date;
                                     $date_array = explode('/', $sheetdata[$i]['A']);
-                                    $transaction_to_save[$i]['year'] = $date_array[0];
-                                    $transaction_to_save[$i]['month'] = $date_array[1];
+                                    $transaction_to_save[$i]['year'] = $date_array[2];
+                                    $transaction_to_save[$i]['month'] = $date_array[0];
 
                                     $transaction_to_save[$i]['ref_no'] = $sheetdata[$i]['B'];
                                     $transaction_to_save[$i]['receipt_no'] = $receipt_no;
@@ -97,15 +94,24 @@ class MigrationController extends AppController{
 
                                     $transaction_to_save[$i]['subtotal'] = $sheetdata[$i]['M'];
                                     $transaction_to_save[$i]['tax'] = $sheetdata[$i]['N'];
-                                    $transaction_to_save[$i]['total'] = $sheetdata[$i]['O'];
+                                    $transaction_to_save[$i]['total'] = $sheetdata[$i]['O']; 
 
-                                    
+                                    $lastTransaction++;
+                                    $currTransactionID = $lastTransaction;
                                 }
-                                
-                                /*
-                                    // get transactionid for transaction
+                                else {
+                                    $currTransactionID = $curr_transaction['Transaction']['id'];
+                                }
 
-                                    $transaction_item_to_save[$i]['transaction_id'] = 0;
+                                //check if current transaction id exists (item already written to database)
+                                $curr_transaction_item = $this->TransactionItem->find('first', array(
+                                    'conditions' => array(
+                                        'TransactionItem.transaction_id' => $currTransactionID,
+                                    ),
+                                    'recursive' => -1,
+                                ));
+                                if (empty($curr_transaction_item)) {
+                                    $transaction_item_to_save[$i]['transaction_id'] = $currTransactionID;
 
                                     $transaction_item_to_save[$i]['description'] = "Being Pament for:".$sheetdata[$i]['K'];
                                     $transaction_item_to_save[$i]['quantity'] = 1;
@@ -113,18 +119,50 @@ class MigrationController extends AppController{
                                     $transaction_item_to_save[$i]['sum'] = $transaction_item_to_save[$i]['quantity'] * $transaction_item_to_save[$i]['unit_price'];
 
                                     $transaction_item_to_save[$i]['table'] = "Member";
-                                    $transaction_item_to_save[$i]['table_id'] = 0;;
+                                    $transaction_item_to_save[$i]['table_id'] = $currMemberID;;
+                                }
+                                else {
 
-                                    */
+                                }
                             }
-                            
-                            //print_r($member_to_save);
-                            //print_r($transaction_to_save);
-                            //print_r($transaction_item_to_save);
+
+                            if (empty($member_to_save)){
+                                //print_r('No Member added!');  //debug use
+                            } else {
+                                if ($this->Member->saveAll($member_to_save)){
+                                    //print_r(count($member_to_save).' Members added!');  //debug use
+                                }
+                                else {
+                                    //print_r('No Member added!');  //debug use
+                                }
+                            }
+
+                            if (empty($transaction_to_save)){
+                                //print_r('No Transaction added!');
+                            } else {
+                                if ($this->Transaction->saveAll($transaction_to_save)){
+                                    //print_r(count($transaction_to_save).' Transanctions added!');  //debug use
+                                }
+                                else {
+                                    //print_r('No Transaction added!');  //debug use
+                                }
+                            }
+
+                            if (empty($transaction_item_to_save)) {
+                                //print_r('No Transaction Item added!');  //debug use
+                            } else {
+                                if ($this->TransactionItem->saveAll($transaction_item_to_save)){
+                                    //print_r(count($transaction_item_to_save).' TransactionItems added!');  //debug use
+                                }
+                                else {
+                                    //print_r('No Transaction Item added!');  //debug use
+                                }
+                            }
+
                         }
                     }
                     else {
-                        $this->setFlash('File Extension should be .csv');
+                        $this->setFlash('File Extension should be .xlsx');
                     }
                 }
             }
